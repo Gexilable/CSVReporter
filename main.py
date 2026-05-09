6813be227116371de6bb4cebdee479e11bd16ad6
Roman <roman@email.com>
2026-05-09 14:23:46 +0300

    refactor read_csv() add typehints

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import csv
+from pathlib import Path
+from typing import List, Dict
 
 
-def read_csv(file):
+def read_csv(file: Path) -> List[Dict] :
     rows = []
+
     with open(file) as csvfile:
         reader = csv.DictReader(csvfile)
 
