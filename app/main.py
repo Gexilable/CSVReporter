944e1b6229a6a841aeaee317d43d638cec97e635
Roman <roman@email.com>
2026-05-10 00:54:52 +0300

    added support for entering multiple files

--- app/main.py
+++ app/main.py
@@ -1,19 +1,17 @@
 import csv
 from pathlib import Path
-from typing import List, Dict
 
 
-def read_csv(file: str | Path) -> List[Dict]:
-    file = Path(file)
-    if not file.exists():
-        raise FileNotFoundError(f"File {file} does not exist")
+def read_csv(*files: str | Path) -> list[dict]:
+    for i in files:
+        file = Path(i)
+        if not file.exists():
+            raise FileNotFoundError(f"File {file} does not exist")
 
     rows = []
-
-    with open(file) as csvfile:
-        reader = csv.DictReader(csvfile)
-
-        for row in reader:
-            rows.append(row)
-
+    for file in files:
+        with open(file) as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                rows.append(row)
     return rows
--- tests/tests.py
+++ tests/tests.py
@@ -24,6 +24,31 @@ def test_read_non_existent_csv_raises_notfound_exception(tmp_path):
     with pytest.raises(FileNotFoundError) as e:
         read_csv(file)
 
+
 def test_read_csv_raises_type_error_on_invalid_type():
     with pytest.raises(TypeError) as e:
-        read_csv(123) # type: ignore[arg-type]
+        read_csv(123)  # type: ignore[arg-type]
+
+
+def test_read_few_csv(tmp_path):
+    first_file = tmp_path / "first.csv"
+    second_file = tmp_path / "second.csv"
+
+    with (
+        open(first_file, "w", encoding='utf-8', newline='') as f1,
+        open(second_file, "w", encoding='utf-8', newline='') as f2,
+    ):
+        writer = csv.writer(f1)
+        writer.writerow(["name", "age"])
+        writer.writerow(["Ivan", "10"])
+
+        writer = csv.writer(f2)
+        writer.writerow(["name", "age"])
+        writer.writerow(["Olga", "12"])
+
+    result = read_csv(first_file, second_file)
+
+    assert result[0]["name"] == "Ivan"
+    assert result[0]["age"] == "10"
+    assert result[1]["name"] == "Olga"
+    assert result[1]["age"] == "12"
