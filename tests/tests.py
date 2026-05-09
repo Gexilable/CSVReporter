6fbbb549b9c8ef88b4570af12920c7d754dd5be1
Roman <roman@email.com>
2026-05-09 15:14:42 +0300

    add test for non exists file

--- tests/tests.py
+++ tests/tests.py
@@ -1,6 +1,9 @@
 import csv
+from pathlib import Path
 
-from main import read_csv
+import pytest
+
+from app.main import read_csv
 
 
 def test_read_one_csv_return_content(tmp_path):
@@ -15,4 +18,9 @@ def test_read_one_csv_return_content(tmp_path):
     assert result[0]["name"] == "Ivan"
     assert result[0]["age"] == "10"
 
+def test_read_non_existent_csv_raises_notfound_exception(tmp_path):
+    file = tmp_path / "test.csv"
 
+    with pytest.raises(FileNotFoundError) as e:
+        read_csv(file)
+        assert e.value == f"File {file} not found"
